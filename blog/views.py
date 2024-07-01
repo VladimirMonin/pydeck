ba887c3e1bcbcc6ec672a9a685bfb3667faaf888
v <v@mail.ru>
2024-07-01 16:59:23 +0500

    feat: добавил страничку about

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+USERS_COUNT = 10
+
+def about(request):
+    return HttpResponse(f'<h1>О блоге</h1><p>Пользователей: {USERS_COUNT}</p>')
