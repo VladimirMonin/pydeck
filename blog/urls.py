2e31e57de698a76a5ea5c3431e5028cd07bec2a1
v <v@mail.ru>
2024-07-01 19:39:07 +0500

    каталог: отображение постов и детальный просмотр поста

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import blog_catalog
+from .views import blog_catalog, post_detail
 
 urlpatterns = [
     path('', blog_catalog, name='blog_catalog'),
+    # Маршрут с конвертером slug для отображения отдельной статьи
+    path('<slug:slug>/', post_detail, name='post_detail'),
 
 ]
