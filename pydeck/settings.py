9f5e7d3bb94443e08e8bd1a3085e13894d6b26b8
v <v@mail.ru>
2024-07-03 17:32:17 +0500

    статика: подключили статику в настройках и сделали стили для футера

--- pydeck/settings.py
+++ pydeck/settings.py
@@ -104,7 +104,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+
+STATICFILES_DIRS = [
+       os.path.join(BASE_DIR, 'static'),  # Путь к статике в корневой директории
+   ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
