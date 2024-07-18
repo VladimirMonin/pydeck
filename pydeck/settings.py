fbb191778c28fc08d7adb5b03cf37ce14dc9e2ac
v <v@mail.ru>
2024-07-18 12:50:03 +0500

    CKEditor: подключение

--- pydeck/settings.py
+++ pydeck/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'ckeditor_uploader',
     'django_extensions',
     'blog',
 ]
@@ -117,3 +119,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- pydeck/urls.py
+++ pydeck/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="main"),
@@ -8,4 +10,8 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     # Подключаем blog.urls
     path("blog/", include("blog.urls")),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
