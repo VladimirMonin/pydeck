1dd94e46d17971c03c0a8883e1cb1a2f0740d7fa
v <v@mail.ru>
2024-07-24 17:04:01 +0500

    админка: кастомная админка постов

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
+from calendar import c
 from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+# Вариант с классом
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Поля, которые будут отображаться в админке
+    list_display = ("title", "author", "published_date", "updated_date")
+    # Поля, по которым можно будет искать
+    search_fields = ("title", "text")
+    # Поля, по которым можно будет фильтровать
+    prepopulated_fields = {"slug": ("title",)}
