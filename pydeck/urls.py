6457fd4d4725fe32a3d5e18a4bc328f07dbf4e25
v <v@mail.ru>
2024-07-24 17:31:45 +0500

    media: добавил в настройки папку и создал urls

--- pydeck/urls.py
+++ pydeck/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="main"),
@@ -9,3 +11,6 @@ urlpatterns = [
     # Подключаем blog.urls
     path("blog/", include("blog.urls")),
 ]
+
+if settings.DEBUG:
+       urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
